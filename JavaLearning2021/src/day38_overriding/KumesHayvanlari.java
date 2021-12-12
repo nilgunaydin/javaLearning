2708ca62026a64951f9aec903dbaea1fda59fc28
Nilgün Aydın <nltlb9597@gmail.com>
2021-12-13 00:26:59 +0300

    pazar commiti

---
+++ JavaLearning2021/src/day38_overriding/KumesHayvanlari.java
@@ -0,0 +1,10 @@
+package day38_overriding;
+
+public class KumesHayvanlari {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaLearning2021/src/day38_overriding/Kuslar.java
@@ -0,0 +1,10 @@
+package day38_overriding;
+
+public class Kuslar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
