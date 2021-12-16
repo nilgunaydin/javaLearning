121c76231dea2dea17116631908c4ab7eeb329a2
Nilgün Aydın <nltlb9597@gmail.com>
2021-12-16 15:06:35 +0300

    41 kere masallh

---
+++ JavaLearning2021/src/day41_errors_garbageCollector/C02.java
@@ -0,0 +1,16 @@
+package day41_errors_garbageCollector;
+
+public class C02 {
+
+	public static void main(String[] args) {
+		
+	System.out.println(C01_Final.pi);
+	
+	// C01_Final.pi=4.13;  pi sayisini static olarak tanimladik ki heryerden rhatlikla ulasabilelim
+	//                                 final yaptik ki isteyen ulasabilsin ama kimse degerini degistiremesin
+	
+	System.out.println(C01_Final.pi);
+
+	}
+
+}
